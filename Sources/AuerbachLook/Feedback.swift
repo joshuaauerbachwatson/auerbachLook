3c2994c5e8278fc81589caef6b68372b65a001c4
Josh Auerbach <joshauerbachwatson@gmail.com>
2024-08-27 18:53:54 -0400

    Oops.  Didn't commit properly last time

---
+++ Sources/AuerbachLook/Feedback.swift
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2021-present, Joshua Auerbach
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIKit
+import MessageUI
+
+// Utility to send feedback, possibly including logs
+
+fileprivate let FeedbackSubjectTemplate = "%@ Feedback"
+fileprivate let FeedbackMessageTemplate = """
+Replace this text with your specific feedback.   If you are reporting a problem, it is best to leave any log attachments
+in place; they are transcripts of events that occurred in %@ games and will help in diagnosing the problem.  If you
+prefer to delete the logs it is your call.
+"""
+
+public class Feedback {
+    // Send feedback.  Return true if mail is enabled on this platform (not a guarantee that mail was sent, since that is
+    // actually up to the user).
+    // The arguments are the app name, the mail destination, and a hosting view controller, which much also implement
+    // the delegate function.  It is up to the host to dismiss the view.
+    public static func send(_ appName: String, dest: String, host: UIViewController & MFMailComposeViewControllerDelegate)
+    -> Bool {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = host
+            mail.setToRecipients([dest])
+            let messageBody = String(format: FeedbackMessageTemplate, appName)
+            mail.setMessageBody(messageBody, isHTML: true)
+            let subject = String(format: FeedbackSubjectTemplate, appName)
+            mail.setSubject(subject)
+            let logs = Logger.getAllLogIndices()
+            if logs.count > 0 {
+                addLog(mail, logs[0])
+            }
+            if logs.count > 1 {
+                addLog(mail, logs[1])
+            }
+            Logger.logPresent(mail, host: host, animated: true)
+            return true
+        } else {
+            return false
+        }
+    }
+
+    // Add a log to the outgoing email
+    private static func addLog(_ mail: MFMailComposeViewController, _ index: Int) {
+        let logPath = Logger.makeLogPath(index)
+        let url = URL(fileURLWithPath: logPath)
+        if let attachmentData = try? Data(contentsOf: url) {
+            mail.addAttachmentData(attachmentData, mimeType: "text/plain", fileName: Logger.LogPrefix + String(index))
+        } else {
+            Logger.log(String(format: "Unable to read data from log with path %@", logPath))
+        }
+    }
+}
---
+++ Sources/AuerbachLook/HelpController.swift
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2021-present, Joshua Auerbach
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIKit
+import WebKit
+import MessageUI
+
+// Controller for a full screen modal view with a web view and a button to dismiss it.  Used to display help texts.
+
+fileprivate let HelpTextBackground = UIColor.white
+fileprivate let HelpViewBackground = UIColor(170, 110, 40)
+fileprivate let SendFeedback = "sendFeedback" /* Internal script name */
+fileprivate let ReturnLabelWidth = CGFloat(150)
+fileprivate let NoEmailTitle = "No Email"
+fileprivate let NoEmailMessage =
+    "Cannot send problem report because email is not configured on this device or is not available to this app"
+fileprivate let HelpExt = "html"
+
+
+public class HelpController: UIViewController {
+    let helpPage : String
+    let email : String
+    let returnText : String
+    let appName : String
+    let returnButton = UIButton()
+    var webView : WKWebView!  // Delayed init (viewDidLoad)
+
+    // Arguments are 1.  The resource name of the help page to display, 2. Text to use in the return button,
+    // 3. The name to use when referring to the app.
+    public init(helpPage: String, email: String, returnText: String, appName: String) {
+        self.helpPage = helpPage
+        self.email = email
+        self.returnText = returnText
+        self.appName = appName
+        super.init(nibName: nil, bundle: nil)
+        modalPresentationStyle = UIModalPresentationStyle.fullScreen
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // Set up view
+    public override func viewDidLoad() {
+        // Background
+        view.backgroundColor = HelpViewBackground
+
+        // Return button
+        returnButton.setTitle(returnText, for: .normal)
+        returnButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        returnButton.backgroundColor = ButtonBackground
+        returnButton.addTarget(self, action: #selector(returnTouched), for: .touchUpInside)
+        returnButton.layer.cornerRadius = 8
+        self.view.addSubview(returnButton)
+
+        // Web view
+        let config = WKWebViewConfiguration()
+        let contentCtl = WKUserContentController()
+        contentCtl.add(self, name: SendFeedback)
+        // TODO probably want "Tips" as well
+        config.userContentController = contentCtl
+        webView = WKWebView(frame: CGRect.zero, configuration: config) // Satisfies delayed init
+        webView.backgroundColor = HelpTextBackground
+        view.addSubview(webView)
+        let path = Bundle.main.url(forResource: helpPage, withExtension: HelpExt)!
+        webView.load(URLRequest(url: path))
+    }
+
+    // Allow view to be rotated.   We will redo the layout each time while preserving all controller state.
+    public override var shouldAutorotate: Bool {
+        get {
+            return true
+        }
+    }
+
+    // Respond to request to do new layout
+    public override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        doLayout()
+    }
+
+    // Support all orientations.   Can layout for portrait or landscape, with tablet or phone type dimensions
+    public override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        get {
+            return .all
+        }
+    }
+
+    // Calculate frames for all the subviews based on orientation
+    func doLayout() {
+        let returnY = safeAreaOf(view).minY + border
+        let returnX = view.bounds.width / CGFloat(2) - ReturnLabelWidth / 2
+        returnButton.frame = CGRect(x: returnX, y: returnY, width: ReturnLabelWidth, height: FixedLabelHeight)
+        let webY = returnButton.frame.maxY + border
+        let webX = border
+        let webWidth = view.bounds.width - 2 * border
+        let webHeight = view.bounds.maxY - border - webY
+        webView.frame = CGRect(x: webX, y: webY, width: webWidth, height: webHeight)
+    }
+
+    // Dismiss view when return button touched
+    @objc func returnTouched() {
+        Logger.logDismiss(self, host: (presentingViewController ?? self), animated: true)
+    }
+}
+
+// Conform to WKScriptMessageHandler and MFMailComposeViewControllerDelegate
+extension HelpController : WKScriptMessageHandler, MFMailComposeViewControllerDelegate {
+    // Dispatch "scripts" to internal functions
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        // Handle scripts
+        switch message.name {
+            // TODO probably want to support "Tips" here also
+        case SendFeedback:
+            if !Feedback.send(appName, dest: email, host: self) {
+                bummer(title: NoEmailTitle, message: NoEmailMessage, host: self)
+            }
+        default:
+            Logger.log("userContentController called with unexpected handler name: " + message.name)
+        }
+    }
+
+    // Implement the delegate function to dismiss the mail client
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        Logger.logDismiss(controller, host: self, animated: false)
+    }
+}
